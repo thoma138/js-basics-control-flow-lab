2b2e384dfd37fca8a05ea93c0b575b32f2ff3614
thoma138 <thomaselliott@live.com>
2022-04-20 13:21:50 +1000

    test complete

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideValue) {
+  if (rideValue <= 400) {
+ return 'This one is on me!';
+  } else if (rideValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideValue >= 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(theCity) {
+ return ((theCity === 'NYC') ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(theTip){
+  switch (theTip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
